4c771e5e8bb76c4dab57139e46f4bb0880bbfea0
Marcin Kaciuba <aldor007@gmail.com>
2020-03-28 22:05:04 +0100

    initial commit

---
+++ apps/photos/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'photos',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/photos'
+};
